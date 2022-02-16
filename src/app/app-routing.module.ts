1565e05f978f9c51326e14dbd82cc4592b1495a0
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-16 17:05:02 +0530

    routing of signup and login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path:'users',
+    loadChildren: () => import('./modules/user/user/user-routing.module').then(m => m.UserRoutingModule),
+  }
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
