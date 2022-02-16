0e0b07eedac1ccc7e4da01a5845abef492b71795
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-16 18:40:42 +0530

    sign-up form design complete

--- src/app/modules/user/sign-up/sign-up.component.ts
+++ src/app/modules/user/sign-up/sign-up.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  public roles: Array<any> = [
+    {
+      id: 'Teacher',
+      value: 'Teacher',
+    },
+    {
+      id: 'Student',
+      value: 'Student',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
