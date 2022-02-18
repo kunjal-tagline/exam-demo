0a993d313efcf7e436e79ef9fc35456b2b587ee8
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-18 12:05:05 +0530

    teacher dashboard component genrate

--- src/app/modules/user/teacher/teacher.module.ts
+++ src/app/modules/user/teacher/teacher.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TeacherRoutingModule } from './teacher-routing.module';
+import { StudentDataComponent } from './student-data/student-data.component';
+import { ViewExamComponent } from './view-exam/view-exam.component';
+import { TeacherDashboardComponent } from './teacher-dashboard/teacher-dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StudentDataComponent,
+    ViewExamComponent,
+    TeacherDashboardComponent
+  ],
   imports: [
     CommonModule,
     TeacherRoutingModule
