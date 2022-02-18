9be08b7c68dfe10a46fd56b1906792273f24cf5e
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-18 18:29:22 +0530

    get data of student list using get() api

--- src/app/interfaces/login.interface.ts
+++ src/app/interfaces/login.interface.ts
@@ -2,3 +2,8 @@ export interface Login {
   email: string;
   password: string;
 }
+
+export interface ShowTeacher{
+  name:string;
+  token:string;
+}
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,15 +1,17 @@
-import { Login } from './../interfaces/login.interface';
+import { Login, ShowTeacher } from './../interfaces/login.interface';
 import { Signup } from './../interfaces/signup.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   private url = environment.URL;
+  public token: any = localStorage.getItem('teacherToken');
   constructor(private httpClient: HttpClient) {}
 
   public getSignUp(signUpData: Signup): Observable<Signup> {
@@ -18,5 +20,13 @@ export class UserService {
 
   public getLogin(loginData: Login): Observable<Login> {
     return this.httpClient.post<Login>(this.url + 'users/Login', loginData);
+  }
+
+  public getTeacherData(): Observable<ShowTeacher> {
+    console.log('this.token :>> ', this.token);
+    const headers = new HttpHeaders().set('access-token', this.token);
+    return this.httpClient.get<ShowTeacher>(this.url + 'dashboard/Teachers', {
+      headers: headers,
+    });
   }
 }
