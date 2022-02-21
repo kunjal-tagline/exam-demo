0d41a0cf545bc69239a646f84bd91103500996b8
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-21 10:28:06 +0530

    verify-student component change file location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { VerifyStudentComponent } from './modules/verify-student/verify-student.component';
+import { VerifyStudentComponent } from './modules/teacher/verify-student/verify-student.component';
 
 @NgModule({
   declarations: [
