003a8e65bc7f2e613fa74df12f99c08b8afd2fa5
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-23 17:09:18 +0530

    exam paper detail show

--- src/app/modules/student/view-exam-paper/view-exam-paper.component.ts
+++ src/app/modules/student/view-exam-paper/view-exam-paper.component.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from 'ngx-toastr';
 import { UserService } from './../../../shared/services/user.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { StudentExamPaperResponse } from 'src/app/shared/interfaces/response.interface';
@@ -9,9 +10,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./view-exam-paper.component.scss'],
 })
 export class ViewExamPaperComponent implements OnInit {
+  public spinner: boolean = true;
+  public examPaperData: Array<any> = [];
+
   constructor(
     private UserService: UserService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -19,6 +24,13 @@ export class ViewExamPaperComponent implements OnInit {
     this.UserService.viewExamPaper(id).subscribe(
       (response: StudentExamPaperResponse) => {
         console.log('response :>> ', response);
+        if (response.statusCode === 200) {
+          this.examPaperData = response.data;
+          this.spinner = false;
+          this.toastrService.success(response.message, 'Sucess');
+        } else {
+          this.toastrService.error(response.message, 'Failed');
+        }
       }
     );
   }
