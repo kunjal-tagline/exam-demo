127295041a86b06d29380cdb54e3b14c5d17b207
kunjal-tagline <kunjal.tagline@gmail.com>
2022-02-24 13:00:00 +0530

    route add in go-back file and header file formate

--- src/app/core/go-back/go-back.component.ts
+++ src/app/core/go-back/go-back.component.ts
@@ -1,20 +1,21 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 @Component({
   selector: 'app-go-back',
   templateUrl: './go-back.component.html',
-  styleUrls: ['./go-back.component.scss']
+  styleUrls: ['./go-back.component.scss'],
 })
 export class GoBackComponent implements OnInit {
+  constructor(private location: Location, private route: Router) {}
 
-  constructor(private location:Location) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  public goBack():void{
+  public goBack(): void {
     if (window.history.length > 1) {
-      this.location.back()
+      this.location.back();
+    } else {
+      this.route.navigate(['/users/login']);
     }
   }
 }
